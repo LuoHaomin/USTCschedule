96947793f250802cef5bff5db1ad9f12e81b6f18
Haoyu <haoyu_t@mail.ustc.edu.cn>
2022-06-18 20:30:20 +0800

    6.18 登录对话框更新

--- app/src/main/java/com/edu/ustc/ustcschedule/dialogs/SignInDialog.java
+++ app/src/main/java/com/edu/ustc/ustcschedule/dialogs/SignInDialog.java
@@ -3,8 +3,13 @@ package com.edu.ustc.ustcschedule.dialogs;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
@@ -21,12 +26,17 @@ public class SignInDialog extends DialogFragment {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.fragment_sign_in, null);
         builder.setTitle(R.string.sign_in)
-                .setView(inflater.inflate(R.layout.fragment_sign_in, null))
+                .setView(view)
                 .setPositiveButton(R.string.sign_in, (dialog, id) -> {
-
+                    EditText idEditText = view.findViewById(R.id.ID);
+                    EditText pwdEditText = view.findViewById(R.id.password);
+                    Toast.makeText(requireContext(), "id:"+idEditText.getText()+";pwd:"+pwdEditText.getText(),Toast.LENGTH_SHORT).show();
                 })
-                .setNegativeButton(R.string.cancel, (dialogInterface, i) -> Objects.requireNonNull(SignInDialog.this.getDialog()).cancel());
+                .setNegativeButton(R.string.cancel, (dialogInterface, i) -> {
+                    Objects.requireNonNull(SignInDialog.this.getDialog()).cancel();
+                });
         // Create the AlertDialog object and return it
         return builder.create();
     }
