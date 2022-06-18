11ef5c7cc5554453c05ef7a28088e48b75ecf3d8
4332001876 <mzfslhm@163.com>
2022-06-18 19:43:22 +0800

    git checkout LHM;git commit

--- app/src/main/java/com/edu/ustc/ustcschedule/fragment/DayListFragment.java
+++ app/src/main/java/com/edu/ustc/ustcschedule/fragment/DayListFragment.java
@@ -7,13 +7,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import com.edu.ustc.ustcschedule.R;
 import com.edu.ustc.ustcschedule.SQL.MainDatabaseHelper;
+import com.edu.ustc.ustcschedule.SQL.MySchedule;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -36,11 +40,34 @@ public class DayListFragment extends Fragment {
 
         MainDatabaseHelper db_helper=new MainDatabaseHelper(getContext());
         SQLiteDatabase db=db_helper.getReadableDatabase();
-        Cursor cursor=db.query("SCHEDULE",new String[]{"_id","IS_FINISH","NAME" ,"START_TIME" ,"END_TIME",
+        Cursor cursor=db.query("SCHEDULE",new String[]{"_id","IS_FINISH","NAME" ,"START_TIME" ,"END_TIME","TIME_LENGTH",
                 "IMPORTANCE" ,"IS_REPEAT" ,"PERIOD" , "PLACE" ,"DESCRIPTION"  } ,"START_TIME>"+day_start_str+" AND END_TIME<"+day_end_str,null,null,null,"START_TIME ASC");
 
         cursor.moveToFirst();
 
+        ConstraintLayout layout=(ConstraintLayout)view.findViewById(R.id.day_list_layout);
+        for(int i=0;i< cursor.getCount();i++)
+        {
+            View schedule_view=inflater.inflate(R.layout.fragment_day_list_item, container, false);
+            MySchedule schedule=new MySchedule(cursor);
+
+            long starting_time=schedule.getStartingTime();
+            long ending_time=schedule.getEndingTime();
+            int height=(int)(Math.abs(ending_time-starting_time))/72000;
+            int pos=(int)(Math.min(starting_time,ending_time)%86400000)/72000;
+
+
+            CardView card=(CardView)schedule_view.findViewById(R.id.lesson_card_day);
+            ConstraintLayout.LayoutParams card_params = (ConstraintLayout.LayoutParams) card.getLayoutParams();
+            card_params.height=(int)((float)card_params.height/100.0*height);
+            //card.setMinimumHeight();
+            card_params.topMargin=(int)((float)card_params.topMargin/100.0*pos);
+
+            layout.addView(schedule_view,card_params);
+
+            cursor.moveToNext();
+        }
+        //layout.addView();
 
 
         return view;
