980f9cc07a1dd45c49dabb2fe47a6ab739b0dc94
4332001876 <mzfslhm@163.com>
2022-06-19 12:30:31 +0800

    git checkout LHM;git commit

--- app/src/main/java/com/edu/ustc/ustcschedule/fragment/DayListFragment.java
+++ app/src/main/java/com/edu/ustc/ustcschedule/fragment/DayListFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -35,7 +36,8 @@ public class DayListFragment extends Fragment {
         long day_end=day_start+86400*1000;
         String day_start_str=Long.toString(day_start);
         String day_end_str=Long.toString(day_end);
-        final SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd", Locale.CHINA);
+        final SimpleDateFormat format_day = new SimpleDateFormat("yyyy/MM/dd", Locale.CHINA);
+        final SimpleDateFormat format_time = new SimpleDateFormat("HH:mm");
 
         View view= inflater.inflate(R.layout.fragment_day_list, container, false);
 
@@ -57,8 +59,8 @@ public class DayListFragment extends Fragment {
             long starting_time=Math.max(schedule.getStartingTime(),day_start);
             long ending_time=Math.min(schedule.getEndingTime(),day_end);
 
-            int height=(int)(Math.abs(ending_time-starting_time))/72000;
-            int pos=(int)(Math.min(starting_time,ending_time)%86400000)/72000;
+            double height=(Math.abs(ending_time-starting_time))/72000;
+            double pos=(Math.min(starting_time,ending_time)%86400000)/72000+6.5;//6是line到layout顶部的高度
 
 
             CardView card=(CardView)schedule_view.findViewById(R.id.lesson_card_day);
@@ -71,12 +73,25 @@ public class DayListFragment extends Fragment {
                 }
             });
             ConstraintLayout.LayoutParams card_params = (ConstraintLayout.LayoutParams) card.getLayoutParams();
+            ((TextView)card.findViewById(R.id.lesson_text_day)).setText(schedule.getName());
+            ((TextView)card.findViewById(R.id.lesson_teacher)).setText(schedule.getDescription());
+            ((TextView)card.findViewById(R.id.lesson_place)).setText(schedule.getPlace());
+            //String starting_time_str=format_time.format(starting_time);
+            //String ending_time_str=format_time.format(ending_time);
+            ((TextView)schedule_view.findViewById(R.id.start_time_text)).setText(format_time.format(starting_time));
+            ((TextView)schedule_view.findViewById(R.id.end_time_text)).setText(format_time.format(ending_time));
+
+
+
             double magnify_ratio=(float)card_params.height/100.0;
             card_params.height=(int)(magnify_ratio*height);//放大倍数乘值
-            //card.setMinimumHeight();
-            card_params.topMargin=pos;//(int)(magnify_ratio*pos);
+            card_params.topMargin=(int)(magnify_ratio*pos);
 
-            layout.addView(schedule_view,card_params);
+            //schedule_view.layout(0,100,schedule_view.getRight()-schedule_view.getLeft(),170);
+            layout.addView(schedule_view);
+            //schedule_view.setLayoutParams(card_params);
+
+
 
             cursor.moveToNext();
         }
