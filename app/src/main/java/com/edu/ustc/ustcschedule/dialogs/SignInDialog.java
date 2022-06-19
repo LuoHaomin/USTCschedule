fcd75c93523fe2b706e782b45dc151c6a9b80c5b
Haoyu <haoyu_t@mail.ustc.edu.cn>
2022-06-19 13:42:04 +0800

    6.19-1

--- app/src/main/java/com/edu/ustc/ustcschedule/dialogs/SignInDialog.java
+++ app/src/main/java/com/edu/ustc/ustcschedule/dialogs/SignInDialog.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,35 +14,26 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.preference.PreferenceManager;
 
 import com.edu.ustc.ustcschedule.R;
 
 import java.util.Objects;
 
+import javax.security.auth.callback.Callback;
+
 public class SignInDialog extends DialogFragment {
-//
-//    /* The activity that creates an instance of this dialog fragment must
-//     * implement this interface in order to receive event callbacks.
-//     * Each method passes the DialogFragment in case the host needs to query it. */
-//    public interface SignInDialogListener {
-//        public void onDialogPositiveClick(DialogFragment dialog);
-//    }
-//
-//    // Use this instance of the interface to deliver action events
-//    SignInDialogListener listener;
-//
-//    // Override the Fragment.onAttach() method to instantiate the SignInDialogListener
+
+    SharedPreferences sharedPreferences = null;
+
 //    @Override
-//    public void onAttach(@NonNull Context context) {
-//        super.onAttach(context);
-//        // Verify that the host activity implements the callback interface
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
 //        try {
-//            // Instantiate the SignInDialogListener so we can send events to the host
-//            listener = (SignInDialogListener) context;
+//            Callback callback = (Callback) getParentFragment();
 //        } catch (ClassCastException e) {
-//            // The activity doesn't implement the interface, throw exception
-//            throw new ClassCastException(context
-//                    + " must implement SignInDialogListener");
+//            throw new ClassCastException("Calling fragment must implement Callback interface");
 //        }
 //    }
 
@@ -53,15 +45,21 @@ public class SignInDialog extends DialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = requireActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.fragment_sign_in, null);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext());
+        @SuppressLint("CommitPrefEdits") SharedPreferences.Editor editor = sharedPreferences.edit();
         builder.setTitle(R.string.sign_in)
                 .setView(view)
                 .setPositiveButton(R.string.sign_in, (dialog, id) -> {
                     EditText idEditText = view.findViewById(R.id.ID);
                     EditText pwdEditText = view.findViewById(R.id.password);
-                    Toast.makeText(requireContext(), "id:"+idEditText.getText()+";pwd:"+pwdEditText.getText(),Toast.LENGTH_SHORT).show();
-//                    listener.onDialogPositiveClick(SignInDialog.this);
+                    editor.putString("id", String.valueOf(idEditText.getText()));
+                    editor.putString("pwd", String.valueOf(pwdEditText.getText()));
+                    editor.apply();
                 })
                 .setNegativeButton(R.string.cancel, (dialogInterface, i) -> {
+                    editor.putString("id", "");
+                    editor.putString("pwd", "");
+                    editor.apply();
                     Objects.requireNonNull(SignInDialog.this.getDialog()).cancel();
                 });
         // Create the AlertDialog object and return it
