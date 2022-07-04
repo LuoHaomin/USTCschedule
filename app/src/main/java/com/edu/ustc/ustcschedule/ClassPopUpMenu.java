03096fccdd43ad2eeafb48c2f9d2b9d2097ead21
Haoyu <haoyu_t@mail.ustc.edu.cn>
2022-07-04 23:16:48 +0800

    7.4-3 筛选菜单

--- app/src/main/java/com/edu/ustc/ustcschedule/ClassPopUpMenu.java
+++ app/src/main/java/com/edu/ustc/ustcschedule/ClassPopUpMenu.java
@@ -1,13 +1,18 @@
 package com.edu.ustc.ustcschedule;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.View;
 import android.widget.PopupMenu;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.preference.PreferenceManager;
+
 import com.edu.ustc.ustcschedule.dialogs.AddCourseDialog;
 import com.edu.ustc.ustcschedule.dialogs.AddTaskDialog;
 import com.edu.ustc.ustcschedule.dialogs.AddTodoDialog;
+import com.edu.ustc.ustcschedule.share.utils.Share;
 
 public class ClassPopUpMenu {
 
@@ -53,38 +58,48 @@ public class ClassPopUpMenu {
 
     @SuppressLint("NonConstantResourceId")
     public void onFilter(View view) {
+        SharedPreferences sharedPreferences = view.getContext().getSharedPreferences("filter", Context.MODE_MULTI_PROCESS);
+        @SuppressLint("CommitPrefEdits") SharedPreferences.Editor editor = sharedPreferences.edit();
         // View当前PopupMenu显示的相对View的位置
         PopupMenu popupMenu = new PopupMenu(view.getContext(), view);
         // menu布局
         popupMenu.getMenuInflater().inflate(R.menu.menu_filter, popupMenu.getMenu());
+        popupMenu.getMenu().findItem(R.id.filter_task).setChecked(sharedPreferences.getBoolean("filter_task", true));
+        popupMenu.getMenu().findItem(R.id.filter_ddl).setChecked(sharedPreferences.getBoolean("filter_ddl", true));
+        popupMenu.getMenu().findItem(R.id.filter_todo).setChecked(sharedPreferences.getBoolean("filter_todo", true));
+        popupMenu.getMenu().findItem(R.id.filter_courses).setChecked(sharedPreferences.getBoolean("filter_courses", true));
+        popupMenu.getMenu().findItem(R.id.filter_least).setChecked(sharedPreferences.getBoolean("filter_least", true));
+        popupMenu.getMenu().findItem(R.id.filter_imp).setChecked(sharedPreferences.getBoolean("filter_imp", true));
+        popupMenu.getMenu().findItem(R.id.filter_most).setChecked(sharedPreferences.getBoolean("filter_most", true));
 
         // menu的item点击事件
         popupMenu.setOnMenuItemClickListener(item -> {
             boolean check = item.isChecked();
             item.setChecked(!check);
             switch (item.getItemId()) {
-                case R.id.filter_courses:
-                    // TODO 修改显示与否
+                case R.id.filter_task:
+                    editor.putBoolean("filter_task", !check);
                     break;
-                case R.id.filter_exercise:
-
-                    break;
-                case R.id.filter_homework:
-
+                case R.id.filter_ddl:
+                    editor.putBoolean("filter_ddl", !check);
                     break;
                 case R.id.filter_todo:
-
+                    editor.putBoolean("filter_todo", !check);
+                    break;
+                case R.id.filter_courses:
+                    editor.putBoolean("filter_courses", !check);
                     break;
                 case R.id.filter_least:
-
+                    editor.putBoolean("filter_least", !check);
                     break;
                 case R.id.filter_imp:
-
+                    editor.putBoolean("filter_imp", !check);
                     break;
                 case R.id.filter_most:
-
+                    editor.putBoolean("filter_most", !check);
                     break;
             }
+            editor.apply();
             return false;
         });
         popupMenu.show();
